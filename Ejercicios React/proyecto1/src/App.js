855b7d94436865fa1b3766369512d3b0f1a6a05a
Kevinjbmartinez <60078799+Kevinjbmartinez@users.noreply.github.com>
2021-06-03 19:52:18 -0600

    kj

--- Ejercicios React/proyecto1/src/App.js
+++ Ejercicios React/proyecto1/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
-import Persona from "./components/Persona";
+import Persona from "./componentes/Persona";
 
 function App() {
 
