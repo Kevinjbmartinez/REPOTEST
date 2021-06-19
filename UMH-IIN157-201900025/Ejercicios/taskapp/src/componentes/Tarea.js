61fc0e9a43ba68e2ac0386b7f1ab2c458e214cd8
Kevinjbmartinez <60078799+Kevinjbmartinez@users.noreply.github.com>
2021-06-19 15:52:45 -0600

    Kevin

---
+++ UMH-IIN157-201900025/Ejercicios/taskapp/src/componentes/Tarea.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  SectionList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function Tarea(props){
+const {data} = props;
+const {id=0,titulo = "Titulo de Tarea", descripcion ="Sin descripcion"} = data;
+
+    return(
+ <View style={estilos.contenedor}> 
+<Text style={estilos.titulo} >{titulo}</Text>
+<Text style={estilos.descripcion}>{descripcion}</Text>
+ </View>
+    )
+
+}
+
+const estilos = StyleSheet.create({
+contenedor:{
+width: '95%',
+height:150,
+padding:8,
+marginLeft:8,
+marginRight:8,
+margin:8,
+marginBottom:8,
+marginTop:8,
+borderRadius:9,
+backgroundColor:'#85C1E9'
+},
+titulo:{
+    fontSize: 18,
+    fontWeight: '500',
+
+},
+
+descripcion:{
+  fontSize: 18,
+  fontWeight: '500',
+
+}
+
+
+});
